8669d33e957aa97c09e501a5a6c5a22c71b17a99
Rudy VIEL <rudy06102001@gmail.com>
2025-05-15 15:32:33 +0200

    debug

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/react-game/',
+  base: '/React-Game/',
   plugins: [react()],
   resolve: {
     alias: {
